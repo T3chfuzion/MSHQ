1623e04f6eb045d3f609fc5dcc69f6fc7d74acbc
T3chfuzion <phogan2886@gmail.com>
2025-04-06 17:28:47 -0500

    Major style updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('header nav ul');
-
+    
     // Toggle the mobile menu visibility on click
     menuToggle.addEventListener('click', function () {
         navLinks.classList.toggle('active');
