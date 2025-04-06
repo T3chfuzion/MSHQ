9dfab938ed77034e707e64ac3fdcfe48fffafa6b
T3chfuzion <phogan2886@gmail.com>
2025-04-06 12:42:06 -0500

    css and js updated

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
+// JavaScript for Mobile Menu Toggle
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('header nav ul');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', e => {
-            e.preventDefault();
-            window.location.href = link.getAttribute('href');
+    // Toggle the mobile menu visibility on click
+    menuToggle.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+    });
+
+    // Close the mobile menu if any link is clicked
+    const navItems = document.querySelectorAll('header nav ul li a');
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navLinks.classList.remove('active');
         });
     });
 });
